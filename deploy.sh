642fb3b78f25d3b3b25af139c134aff40cc7cf05
sugyan <sugi1982@gmail.com>
2013-11-09 12:09:13 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ cd ../
 # rsync -a --delete --exclude=**/perl/pages/* /home/isucon/webapp/ isucon@app1:/home/isucon/webapp/ &
 # rsync -a --delete --exclude=**/perl/pages/* /home/isucon/webapp/ isucon@app2:/home/isucon/webapp/ &
 # wait
-sudo /usr/sbin/supervisorctl reload &
+sudo /usr/bin/supervisorctl reload &
 # ssh isucon@app1 sudo /usr/sbin/supervisorctl reload &
 # ssh isucon@app2 sudo /usr/sbin/supervisorctl reload &
-# wait
+wait
 echo "done!!!"
